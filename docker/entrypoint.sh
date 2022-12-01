18595e22dab7b4ba0d18b3d39737316b36c7fc88
Phillip Gladman <gladman_phillip@bah.com>
2022-11-30 22:31:07 -0500

    editing entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,5 @@
-flask --app /root/sns-app/app.py run --host=0.0.0.0 > /root/sns-app/log.txt 2>&1 &
-gunicorn --bind 0.0.0.0:5000 app:app --daemon --log-file=log.txt
+flask --app /root/sns-app/app.py run --host=0.0.0.0 > /root/sns-app/log.txt 
+# 2>&1 &
+# gunicorn --bind 0.0.0.0:5000 app:app --daemon --log-file=log.txt
 
-sleep infinity
+# sleep infinity
